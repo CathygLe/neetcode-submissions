64a3e49746bee187529323b3c2620a37962a2ab2
CathygLe <156045630+CathygLe@users.noreply.github.com>
2026-05-22 11:25:39 -0700

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        ## up, down, left, right
+        directions = [[0,-1],[0,1], [-1,0],[1,0]]
+
+        islands = 0
+
+        def search(x, y):
+            if x >= ROWS or y >= COLS or x < 0 or y < 0 or grid[x][y] == "0":
+                return 
+            
+            for xdirection, ydirection in directions:
+                grid[x][y] = "0"
+                search(x + xdirection, y + ydirection)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+
+                if grid[r][c] == "1":
+                    search(r,c)
+                    islands += 1
+
+        return islands
