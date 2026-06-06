694dc0250f18db8b269987b0e750bce7783f7102
CathygLe <156045630+CathygLe@users.noreply.github.com>
2026-06-06 11:00:02 -0700

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited ={}
+
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in visited:
+                return [visited[diff], i]
+
+            else:
+                visited[nums[i]] = i  
+        
+
+            
