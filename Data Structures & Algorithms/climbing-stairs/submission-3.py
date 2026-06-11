3fe41aa283469fa8276c5ac48af06fcefa95241e
CathygLe <156045630+CathygLe@users.noreply.github.com>
2026-06-10 22:41:51 -0700

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # 1 = 1                                            1
+        # 2 = 11, 2                                        2 
+        # 3 = 12, 21, 111                                  3 
+        # 4 = 1111, 22, 112, 211, 121,                     5 
+        # 5 = 1111, 221, 122, 212, 1112, 1121, 1211, 2111, 8
+
+        first = 1
+        second = 1 
+
+        for i in range(n-1):
+            temp = first + second 
+            first = second 
+            second = temp 
+        return second 
